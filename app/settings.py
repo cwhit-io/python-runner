d110b77f84f2171f579ad4c9868990bef448fcad
Christian <c.whitmer@outlook.com>
2026-01-31 00:33:43 -0500

    chore: Update database path in settings and add .gitkeep to db directory

--- app/settings.py
+++ app/settings.py
@@ -117,7 +117,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / 'db' / "db.sqlite3",
     }
 }
 
